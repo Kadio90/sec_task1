0cdf22b60c1861fd92a77cc0d84236a014f6e67c
Anton Zyurkalov <kadio90@mail.ru>
2024-08-01 14:36:39 +0500

    First Commit

---
+++ src/main/java/ru/task1/sec_task1/SecTask1Application.java
@@ -0,0 +1,13 @@
+package ru.task1.sec_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/task1/sec_task1/SecTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.task1.sec_task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
